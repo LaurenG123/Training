aa855e454a31c53fc597aa4149be2d397a87d963
Lauren G <72687468+LaurenG123@users.noreply.github.com>
2023-08-31 18:02:58 +0100

    Add files via upload

---
+++ Roman Numerals.py
@@ -0,0 +1,13 @@
+roman = input("Enter roman numeral string: ")
+
+def romanNum(s):
+    dict = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500}
+    int_val = 0
+    for i in range(len(s)):
+        if i > 0 and dict[s[i]] > dict[s[i-1]]:
+            int_val += dict[s[i]] - 2 * dict[s[i-1]]
+        else:
+            int_val += dict[s[i]]
+    return int_val
+
+print(romanNum(roman))
